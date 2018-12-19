7059a05453dfdefae1187a8793a76bff5bc4c47c
Вячеслав Шилин <slavikshilin@gmail.com>
2018-12-19 13:23:26 +0300

    add REDUX_DEVTOOLS_EXTENSION

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,4 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { rootReducer } from '../reducers'
 import thunk from 'redux-thunk'
 
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const store = createStore(
+    rootReducer, 
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
